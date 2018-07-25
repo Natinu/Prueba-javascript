c487a1e71901a41471619ec17a2f9877e4d0834f
nataly <natimedina.nu@gmail.com>
2018-07-25 15:42:59 -0400

    html y css listo

--- public/js/app.js
+++ public/js/app.js
@@ -75,7 +75,17 @@ module.exports = __webpack_require__(2);
 /* 1 */
 /***/ (function(module, exports) {
 
-alert("Hello! I am an alert box!!");
+$(document).ready(function () {
+	$(".sidebar__form").on('submit', function (e) {
+		var text = $(".sidebar__form--cajadetexto").val();
+		event.preventDefault();
+		$(".main-central").append('<div class="main__row row col-md 12">' + '<div class="main__tweet">' + '<div class="main__tweet--pin">' + '<i class="fas fa-times"></i>' + '</div>' + text + '<div class="main__tweet--features">' + '<i class="fas fa-heart"></i>' + '</div>' + '</div>' + '</div>');
+	});
+
+	$(".main__tweet--pin").on('click', function () {
+		alert("borrando pin");
+	});
+});
 
 /***/ }),
 /* 2 */
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-//alert("Hello! I am an alert box!!");
+$(document).ready(function(){
+	$(".sidebar__form").on('submit', function(e){
+		var text = $(".sidebar__form--cajadetexto").val();
+		event.preventDefault();
+		$(".main-central").append(
+			'<div class="main__row row col-md 12">'
+			+'<div class="main__tweet">'
+			+'<div class="main__tweet--pin">'
+			+'<i class="fas fa-times"></i>'
+			+'</div>'
+			+ text
+			+'<div class="main__tweet--features">'
+			+'<i class="fas fa-heart"></i>'
+			+'</div>'
+			+'</div>'
+			+'</div>')
+
+
+	}); 
+
+$(".main__tweet--pin").on('click', function(){
+	alert("borrando pin");
+
+})
+
+
+});
